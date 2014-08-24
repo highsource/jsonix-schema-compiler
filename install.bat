de983682ef7dd4fafeb28afb7fba074d7b00e5a2
highsource <aleksei.valikov@gmail.com>
2014-08-24 16:10:07 +0200

    Fixed builds, added a few ignores.

--- install.bat
+++ install.bat
@@ -1,17 +1,10 @@
 setlocal
 echo Setting JAVA_HOME to %JAVA6_HOME%.
 set JAVA_HOME=%JAVA6_HOME%
-
 echo Performing a short clean build.
-pause
 call mvn clean install -DperformRelease
 echo Short clean build completed.
-pause
-
 echo Performing a full clean build.
-pause
 call mvn clean install -DperformRelease -Ptests
 echo Full clean build completed.
-pause
-
 endlocal
